22c751e7dcd42e8ce0dca4f1068970798accd24a
rexoliveira <roolivei@yahoo.com.br>
2022-07-21 16:09:33 -0300

    correções conexão banco

--- php/conexao.php
+++ php/conexao.php
@@ -3,10 +3,9 @@ $servidor = "localhost";
 $porta = 5432;
 $bancoDeDados = "usuarios";
 $usuario = "postgres";
-$senha = "postgres";
+$senha = "12070205";
 
-$conexao = pg_connect("host=$servidor port=$porta dbname=$bancoDeDados " +
-    "user=$usuario password=$senha");
+$conexao = pg_connect("host=$servidor port=$porta dbname=$bancoDeDados user=$usuario password=$senha");
 if (!$conexao) {
     die("Não foi possível se conectar ao banco de dados.");
 }
