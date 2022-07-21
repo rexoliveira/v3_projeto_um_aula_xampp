b0ccb62bbd5f6bfa22cb31c93811eed35c686d77
rexoliveira <roolivei@yahoo.com.br>
2022-07-21 14:12:00 -0300

    inicio configurações php/v3/xampp

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php include_once('index.html'); ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <!-- Redirecionado para página principal.html -->
+    <meta http-equiv="refresh" content="0; URL='page/principal.php'"/>
+</head>
+<body>
+    
+</body>
+</html>
