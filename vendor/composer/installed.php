7ed36fca14f85a08cc0ee306331796432e01e082
rexoliveira <roolivei@yahoo.com.br>
2022-07-22 00:37:37 -0300

    update composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'v2_projeto_um_aula/site',
+        'name' => 'v3_projeto_um_aula_xampp/site',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '3a4250249bffdd29265c2a0f6bd05ad2c52ac875',
+        'reference' => '94ac2062c2ffa2581e0228b12a8eea84c1093514',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'v2_projeto_um_aula/site' => array(
+        'v3_projeto_um_aula_xampp/site' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '3a4250249bffdd29265c2a0f6bd05ad2c52ac875',
+            'reference' => '94ac2062c2ffa2581e0228b12a8eea84c1093514',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
