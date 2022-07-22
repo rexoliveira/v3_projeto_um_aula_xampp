04dfc7b0a8a82bc5440a4ba595f89ee593604fde
rexoliveira <roolivei@yahoo.com.br>
2022-07-21 23:21:00 -0300

    ajustes de styles

--- page/usuario.php
+++ page/usuario.php
@@ -21,17 +21,17 @@ pg_close($conexao) ?>
     <!-- fonte de ícones -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 
-    <link rel="stylesheet" href="../style/style.css" />
-    <link rel="stylesheet" href="../style/bot_style.css" />
+    <link rel="stylesheet" href="..\\style\\style.css" />
+    <link rel="stylesheet" href="..\\style\\bot_style.css" />
 
-    <link rel="stylesheet" href="../style/menu_lateral_style.css" />
+    <link rel="stylesheet" href="..\\style\\menu_lateral_style.css" />
 
-    <link rel="stylesheet" href="../style/usuarios_cards.css">
+    <link rel="stylesheet" href="..\\style\\usuarios_cards.css">
 
     <title>Usuários</title>
 </head>
 
-<body class="body">
+<body class=" body">
     <!-- Cabeçalho -->
     <header class="header">
 
@@ -68,14 +68,14 @@ pg_close($conexao) ?>
         <section class="formF">
             <form id="form_filtro" action="" method="get">
                 <label id="label_filtro" for="filtro">Filtrar por</label>
-                <select name="palavraFiltro" id="palavra">
+                <select class="palavraFiltro" name="palavraFiltro" id="palavra">
                     <option value="todos">todos</option>
                     <option value="email">E-mail</option>
                     <option value="nome">Nome</option>
                     <option value="sobrenome">Sobrenome</option>
                     <option value="tel">Celular</option>
                 </select>
-                <input type="text" name="filtro" id="filtro" autofocus>
+                <input class="filtro" type="text" name="filtro" id="filtro" autofocus>
                 <input class="botao_filtro" type="submit" value="Filtrar">
             </form>
         </section>
@@ -88,8 +88,10 @@ pg_close($conexao) ?>
         } elseif ($where == 'tel') {
             $n_filtro = "Celular";
         }
-        if ($filtro != "") {
+        if ($filtro != "" && $where != "todos") {
             print "<h5>Resultado do filtro \"$n_filtro\" contendo \"$filtro\". </h5>";
+        }else{
+            print "<h5>Escolha outro filtro no lugar de \"todos\" para uma informação específica. </h5>";
         }
         print '<section class="l-cards">';
         /* print "<h2>$registros resgistros encontrados.</h2>" */
@@ -107,14 +109,14 @@ pg_close($conexao) ?>
             print "<h3>E-mai: $email - Celular: $tel</h3>"; */
             print "<article class=\"c-card\">";
             print "<section class=\"c-card_image\">";
-            print "<img src=\"https://as1.ftcdn.net/v2/jpg/01/71/25/36/1000_F_171253635_8svqUJc0BnLUtrUOP5yOMEwFwA8SZayX.jpg\" width=\"100%\" alt=\"image placeholder\">";
+            print "<img class=\"imagem\"src=\"https://as1.ftcdn.net/v2/jpg/01/71/25/36/1000_F_171253635_8svqUJc0BnLUtrUOP5yOMEwFwA8SZayX.jpg\" alt=\"image placeholder\">";
             print "</section>";
             print "<section class=\"c-card_content\">";
 
-            print "<span class=\"u-text-placeholder\" style=\"width: 30%\"><h2>Nome: $nome</h2></span>";
+            print "<span class=\"u-text-placeholder\" style=\"width: 30%\"><h2>$nome</h2></span>";
 
-            print " <span class=\"u-text-placeholder\" style=\"width: 85%\"><h2>E-mail: $email</h2></span>";
-            print "<span class=\"u-text-placeholder\" style=\"width: 60%\"><h2>Telefone: $tel</h2></span>";
+            print " <span class=\"u-text-placeholder\" style=\"width: 85%\"><h2>$email</h2></span>";
+            print "<span class=\"u-text-placeholder\" style=\"width: 60%\"><h2>$tel</h2></span>";
             print "</section>";
             print "</article>";
         }
