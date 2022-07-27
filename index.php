d70db1f5096d14cb721c0f41b9688dddb90d9fd4
rexoliveira <roolivei@yahoo.com.br>
2022-07-27 13:34:50 -0300

    Retirado full inicial para mais testes e ajustes

--- index.php
+++ index.php
@@ -3,16 +3,19 @@
 
 <head>
     <!-- Redirecionado para página principal.html -->
-    <!-- <meta http-equiv="refresh" content="0; URL='/parte_dash_figma/dash_figma.php'" /> -->
-    <script>
-    function fullwin() {
-        window.open("parte_dash_figma/dash_figma.php", "", "fullscreen")
-    }
-    </script>
+    <meta http-equiv="refresh" content="0; URL='/parte_dash_figma/dash_figma.html'" />
+    <--INICIAR A TELA FULL FOI DESATIVADA PARA MAIS TESTES A AJUSTES-->
+        <--Crédito tela full:http://phpbrasil.com/phorum/read.php?1,58112-->
+            <!--   <script>
+        function fullwin() {
+            window.open("parte_dash_figma/dash_figma.php", "", "fullscreen")
+        }
+        </script> -->
 </head>
 
-<body onLoad="javascript:fullwin()">
+<!-- <body onLoad="javascript:fullwin()"> -->
 
+<body>
 </body>
 
 </html>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'v3_projeto_um_aula_xampp/site',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '94ac2062c2ffa2581e0228b12a8eea84c1093514',
+        'reference' => 'f30fc2c2d292ac304ef2d776099bef0e41697077',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'v3_projeto_um_aula_xampp/site' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '94ac2062c2ffa2581e0228b12a8eea84c1093514',
+            'reference' => 'f30fc2c2d292ac304ef2d776099bef0e41697077',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
