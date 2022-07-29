5cb9f4342689b30f8de8119b14260214f70bbe34
rexoliveira <rex.oliveira15@gmail.com>
2022-07-29 20:27:10 -0300

    Ajuste no menu lateral

--- parte_dash_figma/menu_topo.js
+++ parte_dash_figma/menu_topo.js
@@ -1,6 +1,11 @@
+
+const hea = document.querySelector('header')
+
 $(function () {
  $('.toggle-menu').click(function(){
 	$('.exo-menu').toggleClass('display');
+
+	hea.style.width = '100%'
 	
  });
  
