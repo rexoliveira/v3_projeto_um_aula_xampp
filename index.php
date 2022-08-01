f43fcbde29f3be976d7cbfa8348c09f9aae0fe79
rexoliveira <roolivei@yahoo.com.br>
2022-08-01 19:27:42 -0300

    Ajuuste no index

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 <head>
     <!-- Redirecionado para página principal.html -->
     <meta http-equiv="refresh" content="0; URL='/parte_dash_figma/dash_figma.html'" />
-    <--INICIAR A TELA FULL FOI DESATIVADA PARA MAIS TESTES A AJUSTES-->
-        <--Crédito tela full:http://phpbrasil.com/phorum/read.php?1,58112-->
-            <!--   <script>
+    <!--INICIAR A TELA FULL FOI DESATIVADA PARA MAIS TESTES A AJUSTES-->
+    <!--Crédito tela full:http://phpbrasil.com/phorum/read.php?1,58112-->
+    <!--   <script>
         function fullwin() {
             window.open("parte_dash_figma/dash_figma.php", "", "fullscreen")
         }
