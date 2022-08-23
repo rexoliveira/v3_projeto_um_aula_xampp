d87467bb555ebe8108027521311762429aa37792
rexoliveira <roolivei@yahoo.com.br>
2022-08-22 21:19:01 -0300

    Ajustes PDO

--- parte_dash_figma/usuarios_port/conexao.php
+++ parte_dash_figma/usuarios_port/conexao.php
@@ -19,7 +19,6 @@ $porta = 5432;
 $bancoDeDados = "d8r7j0jch56qj4";
 $usuario = "skmpjtptrfsukp";
 $senha = "48fc0ade9ef49d7f2742a0ce5498e7f82ba47f699bb0133cb5a0a07a361c4786";
-$conexao = "";
 try {
     $dsn = "pgsql:host=$servidor;port=$porta;dbname=$bancoDeDados;";
 
