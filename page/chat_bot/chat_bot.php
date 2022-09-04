43c95cf7e7d2911d4f1e2ff5768f163a1bb8d078
rexoliveira <roolivei@yahoo.com.br>
2022-09-04 01:46:30 -0300

    Inicia ChatBot Dialogflow do Google

---
+++ page/chat_bot/chat_bot.php
@@ -0,0 +1,5 @@
+<body id="chat_bot">
+<iframe  allow="microphone;" width="310" height="400"
+    src="https://console.dialogflow.com/api-client/demo/embedded/85968218-b8a2-4443-96c3-d4c3a0d35e2a">
+</iframe>
+</body>
