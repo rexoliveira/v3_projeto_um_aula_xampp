f93b036fa41b382218311a167ff1b93b61dcd6af
rexoliveira <roolivei@yahoo.com.br>
2022-09-07 12:18:33 -0300

    Ajustes de responsividade e scroll

--- page/menu_topo/images-drop-down.php
+++ page/menu_topo/images-drop-down.php
@@ -1,37 +1,22 @@
 <li class="images-drop-down">
-            <a href="#"><i class="fa fa-photo"></i> Cultura</a>
-            <div class="Images animated fadeIn">
-              <div class="col-md-3">
-                <h4>Secretaria da Cultura</h4>
-                <!-- Original: src="https://2.bp.blogspot.com/-VG_e0pKfrDo/VcLb6JwZqfI/AAAAAAAAGCk/8ZgA9kZqTQ8/s1600/images3.jpg" -->
-                <img
-                  class="img-responsive"
-                  src="../image/cultura/secretaria-cultura .jpg"
-                />
-              </div>
-              <div class="col-md-3">
-                <h4>Feira do Livro</h4>
-                <!--src="https://3.bp.blogspot.com/-hUt5FrdZHio/VcLb5dlwTBI/AAAAAAAAGCU/UUH5N1JkoQc/s1600/images1.jpg"  -->
-                <img
-                  class="img-responsive"
-                  src="../image/cultura/feira-livro.jpg"
-                />
-              </div>
-              <!-- src="https://4.bp.blogspot.com/-A7U1uPlSq6Y/VcLb5kKHCkI/AAAAAAAAGCc/7WghyndTEuY/s1600/images2.jpg"   -->
-              <div class="col-md-3">
-                <h4>53ª Expocamaqua e 3ª Mostra da Agricultura Familiar</h4>
-                <img
-                  class="img-responsive"
-                  src="../image/cultura/53-expocamaqua-e-3-mostra-da-agricultura-familiar.jpg"
-                />
-              </div>
-              <div class="col-md-3">
-                <h4>Desfile da Patria 2019</h4>
-                <!-- src="https://3.bp.blogspot.com/-hGrnZIjzL2k/VcLb47kyQKI/AAAAAAAAGCQ/J6Q2IAHIQvQ/s1600/image4.jpg"  -->
-                <img
-                  class="img-responsive"
-                  src="../image/cultura/desfile-da-patria-2019.jpg"
-                />
-              </div>
-            </div>
-          </li>
+  <a href="#"><i class="fa fa-photo"></i> Cultura</a>
+  <section class="Images animated fadeIn">
+    <section class="col-md-3">
+      <h4>Secretaria da Cultura</h4>
+      <img class="img-responsive" src="../image/cultura/secretaria-cultura .jpg" />
+    </section>
+    <section class="col-md-3">
+      <h4>Feira do Livro</h4>
+
+      <img class="img-responsive" src="../image/cultura/feira-livro.jpg" />
+    </section>
+    <section class="col-md-3">
+      <h4>53ª Expocamaqua e 3ª Mostra da Agricultura Familiar</h4>
+      <img class="img-responsive" src="../image/cultura/53-expocamaqua-e-3-mostra-da-agricultura-familiar.jpg" />
+    </section>
+    <section class="col-md-3">
+      <h4>Desfile da Patria 2019</h4>
+      <img class="img-responsive" src="../image/cultura/desfile-da-patria-2019.jpg" />
+    </section>
+  </section>
+</li>
