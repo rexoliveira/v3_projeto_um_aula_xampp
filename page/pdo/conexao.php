0d5c27a3c9083f629b1f275123d3c514b6396dd8
rexoliveira <roolivei@yahoo.com.br>
2022-09-12 20:47:18 -0300

    Merge branch 'master' of https://github.com/rexoliveira/v3_projeto_um_aula_xampp

--- page/pdo/conexao.php
+++ page/pdo/conexao.php
@@ -1,15 +1,12 @@
 <?php
- $servidor = "ec2-3-219-229-143.compute-1.amazonaws.com";
-$porta = 5432;
-$bancoDeDados = "d8r7j0jch56qj4";
-$usuario = "skmpjtptrfsukp";
-$senha = "48fc0ade9ef49d7f2742a0ce5498e7f82ba47f699bb0133cb5a0a07a361c4786";
-/*$servidor = "127.0.0.1";
-$porta = 5432;
-$bancoDeDados = "usuarios";
-$usuario = "postgres";
-//$senha = "postgres";//Banco IFSUL
-$senha = "12070205";*/
+require_once("config.php");
+
+$servidor = DB_HOST;
+$porta = DB_PORT;
+$bancoDeDados = DB_DATABASE_NAME;
+$usuario = DB_USERNAME;
+$senha = DB_PASSWORD;
+
 try {
     $dsn = "pgsql:host=$servidor;port=$porta;dbname=$bancoDeDados;";
 $conexao = new PDO($dsn, $usuario, $senha, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
