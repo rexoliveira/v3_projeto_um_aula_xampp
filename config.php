6999f39130dbb9e4774cf6394de5c979c710730e
rexoliveira <roolivei@yahoo.com.br>
2022-09-14 23:00:48 -0300

    Ajuste path config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+//SERVIDOR HEROKU BANCO
+define ("DB_HOST", "ec2-3-219-229-143.compute-1.amazonaws.com");
+define("DB_PORT","5432");
+define("DB_DATABASE_NAME","d8r7j0jch56qj4");
+define("DB_USERNAME","skmpjtptrfsukp");
+define("DB_PASSWORD","48fc0ade9ef49d7f2742a0ce5498e7f82ba47f699bb0133cb5a0a07a361c4786");
+
+//BANCO IFSUL
+/* define ("DB_HOST", "127.0.0.1");
+define("DB_PORT","5432");
+define("DB_DATABASE_NAME","usuarios");
+define("DB_USERNAME","postgres");
+define("DB_PASSWORD","postgres"); */
+
+//BANCO CASA
+/* define ("DB_HOST", "127.0.0.1");
+define("DB_PORT","5432");
+define("DB_DATABASE_NAME","usuarios");
+define("DB_USERNAME","postgres");
+define("DB_PASSWORD","12070205"); */
